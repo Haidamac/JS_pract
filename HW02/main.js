6c11e202dc4faf252f2223891fcb905fe21cf059
Serge Krynytsia <113309864+Haidamac@users.noreply.github.com>
2023-12-18 19:31:17 +0200

    homework 2.2 completed

--- HW02/main.js
+++ HW02/main.js
@@ -7,3 +7,25 @@ console.log(arr);
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+let book1 = {
+    title: "Yuval Noah Harari. Homo Deus",
+    pageCount: 464,
+    genre: "social philosophy"
+};
+
+let book2 = {
+    title: "Mark Manson. The Subtle Art of Not Giving a F*ck",
+    pageCount: 160,
+    genre: "motivating"
+};
+
+let book3 = {
+    title: "Ray Bradbury. A Sound of Thunder",
+    pageCount: 250,
+    genre: "science fiction"
+};
+
+console.log(book1, book2, book3);
