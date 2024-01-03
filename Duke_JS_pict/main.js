060be05c7d54261033329f0a75c7ebd2a9faa737
Serge Krynytsia <113309864+Haidamac@users.noreply.github.com>
2024-01-03 19:49:47 +0200

    add change color

--- Duke_JS_pict/main.js
+++ Duke_JS_pict/main.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     console.log(w, h, pixel);
 });
+
+// change color
+var img = new SimpleImage(200,200);
+
+for (let pixel of img.values()) {
+    let NewRed = pixel.getRed() + 255;
+    let NewGreen = pixel.getGreen() + 255;
+    pixel.setRed(NewRed);
+    pixel.setGreen(NewGreen);
+}
+
+print(img);
